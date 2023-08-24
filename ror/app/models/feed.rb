e20b3b0ad74386ad00a4411a04ecdb8d8417374e
Andrew G <36536011+gukandrew@users.noreply.github.com>
2023-08-25 01:13:11 +0300

    Add trigger_active! into Feed & schedule it via whenever

--- ror/app/models/feed.rb
+++ ror/app/models/feed.rb
@@ -1,2 +1,9 @@
 class Feed < ApplicationRecord
+  has_many :items, dependent: :destroy
+
+  def self.trigger_active!
+    urls = Feed.where(active: true).pluck(:url)
+
+    Bunny::UrlsTrigger.call(urls)
+  end
 end
