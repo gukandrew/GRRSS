39608e3b6d0d465474d9ff1cdc6107c574441c97
Andrew G <36536011+gukandrew@users.noreply.github.com>
2023-08-24 13:12:55 +0300

    Implement Reader service

--- rss_service/main.go
+++ rss_service/main.go
@@ -1,12 +1,115 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
+	"log"
 	"runtime"
+	"time"
+
+	reader "main/reader"
+
+	amqp "github.com/rabbitmq/amqp091-go"
 )
+
+var conn *amqp.Connection
+var chIn *amqp.Channel
+var qIn amqp.Queue
+var chOut *amqp.Channel
+var qOut amqp.Queue
+
+type ResponseJSON struct {
+	Items []reader.RssItem `json:"items"`
+}
 
 func main() {
 	hello()
+
+	var err error
+	conn, err = amqp.Dial("amqp://grrss:grrss_secret@localhost:5672")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	chIn, qIn = prepareChannel(conn, "service_rss_feed_in")
+	chOut, qOut = prepareChannel(conn, "service_rss_feed_out")
+
+	defer chIn.Close()
+	defer chOut.Close()
+	listenChannel(chIn, qIn)
+
+	for {
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func prepareChannel(conn *amqp.Connection, name string) (*amqp.Channel, amqp.Queue) {
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+
+	q, err := ch.QueueDeclare(
+		name, // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+	return ch, q
+}
+
+func listenChannel(ch *amqp.Channel, q amqp.Queue) {
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	failOnError(err, "Failed to register a consumer")
+
+	go func() {
+		for d := range msgs {
+			log.Printf("Received a message: %s", d.Body)
+
+			var urls []string
+			json.Unmarshal(d.Body, &urls)
+
+			items := reader.Parse(urls)
+			body, _ := json.Marshal(ResponseJSON{ Items: items })
+			log.Printf("Processed %v items!", len(items))
+
+			publishMessage(chOut, qOut, string(body))
+		}
+	}()
+
+	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+}
+
+func publishMessage(ch *amqp.Channel, q amqp.Queue, body string) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err := ch.PublishWithContext(ctx,
+		"",     // exchange
+		q.Name, // routing key
+		false,  // mandatory
+		false,  // immediate
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		})
+	failOnError(err, "Failed to publish a message")
+	log.Print(" [x] Sent result!\n")
+}
+
+func failOnError(err error, msg string) {
+  if err != nil {
+    log.Panicf("%s: %s", msg, err)
+  }
 }
 
 func hello() {
---
+++ rss_service/reader/main.go
@@ -0,0 +1,53 @@
+package reader
+
+import (
+	"time"
+
+	gofeed "github.com/mmcdole/gofeed"
+)
+
+type RssItem struct {
+	Title				string    `json:"title"`
+	Source			string    `json:"source"`
+	SourceURL		string    `json:"source_url"`
+	Link				string    `json:"link"`
+	PublishDate time.Time `json:"publish_date"`
+	Description string    `json:"description"`
+}
+
+func Parse(urls []string) []RssItem {
+	var items []RssItem
+	for _, url := range urls {
+		items = append(items, parseFeed(url)...)
+	}
+	return items
+}
+
+// parseURL is a variable that can be mocked in tests
+var parseURL = func(fp *gofeed.Parser, url string) (feed *gofeed.Feed, err error) {
+	return fp.ParseURL(string(url))
+}
+
+func parseFeed(url string) []RssItem {
+	fp := gofeed.NewParser()
+	fp.UserAgent = "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/116.0"
+	feed, err := parseURL(fp, url)
+
+	if err != nil {
+		panic(err)
+	}
+
+	var items []RssItem
+	for _, item := range feed.Items {
+		items = append(items, RssItem{
+			Title: item.Title,
+			Source: feed.Title,
+			SourceURL: url,
+			Link: item.Link,
+			PublishDate: *item.PublishedParsed,
+			Description: item.Description,
+		})
+	}
+
+	return items
+}
