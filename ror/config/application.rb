ffe88c85b53aca9006a52011014491d1192c76b2
Andrew G <36536011+gukandrew@users.noreply.github.com>
2023-08-28 21:54:49 +0300

    Add ability to set whitelisted hosts ro RoR app

--- ror/config/application.rb
+++ ror/config/application.rb
@@ -19,6 +19,8 @@ module Grrss
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
+    config.hosts = ENV['WHITELISTED_HOSTS'].split(',') || [] if ENV['WHITELISTED_HOSTS'].present?
+
     config.autoload_paths << "#{config.root}/app/consumers"
     config.autoload_paths << "#{config.root}/app/services"
   end
