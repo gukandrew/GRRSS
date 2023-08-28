b84444cda68fb5512e0f96e03a5a3c5d2b30ca7d
Andrew G <36536011+gukandrew@users.noreply.github.com>
2023-08-28 12:24:35 +0300

    Add Filtering & Ordering ability for items

--- ror/app/controllers/api/items_controller.rb
+++ ror/app/controllers/api/items_controller.rb
@@ -1,7 +1,27 @@
 class Api::ItemsController < ApplicationController
   def index
-    @records = Item.all.order(published_at: :desc)
+    @records = Item.all.order(published_at: order_by)
+    @records = @records.where(feed_id: selected_feed_ids) if selected_feed_ids.present?
 
     render json: { success: 1, records: @records }
+  end
+
+  private
+
+  def order_by
+      case filter_params[:order_by]
+      when 'asc'
+        :asc
+      else
+        :desc
+      end
+  end
+
+  def selected_feed_ids
+    filter_params[:feed_ids]
+  end
+
+  def filter_params
+    params.permit(:order_by, feed_ids: [])
   end
 end
